1d1fa1ce85fe69c5d28375e4d596e5ea596e1ac8
andy <a5706617@gmail.com>
2023-02-06 20:30:14 +0800

    commit

--- note-app/script.js
+++ note-app/script.js
@@ -12,7 +12,23 @@ addBtn.addEventListener("click", () =>{
 
 })
 
+function addNewNote(text = "") {
+    const note = document.createElement("div");
+    note.classList.add("note");
 
+    note.innerHTML = `
+        <div class="notes">
+            <div class="tools">
+                <button class="edit"><i class="fas fa-edit"></i></button>
+                <button class="delete"><i class="fas fa-trash-alt"></i></button>
+            </div>
+            <div class="main ${text ? "" : "hidden"}"></div>
+            <textarea class="${text ? "hidden" : ""}"></textarea>
+        </div>
+    `;
+
+    
+}
 
 
 
