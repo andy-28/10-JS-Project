e2882adaf3f59c32c417e779db956a259bd01011
andy <a5706617@gmail.com>
2023-02-06 20:34:10 +0800

    coomit

--- note-app/script.js
+++ note-app/script.js
@@ -33,7 +33,13 @@ function addNewNote(text = "") {
     const main = note.querySelector(".main");
     const textArea = note.querySelector(".textarea");
 
-    
+    textArea.value = text;
+    main.innerHTML = marked(text);
+
+    editBtn.addEventListener("click", () => {
+        main.classList.toggle("hidden");
+        textArea.classList.toggle("hidden");
+    });
 
 }
 
