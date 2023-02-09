cb14f9771829b6b3d636e9e3d8c6ed76b42e173d
andy <a5706617@gmail.com>
2023-02-09 22:16:03 +0800

    commit

--- todo/script.js
+++ todo/script.js
@@ -6,7 +6,7 @@ const todos = JSON.parse(localStorage.getItem("todos"));
 
 if (todos) {
     todos.forEach((todo) => {
-        
+        addTodo(todo);
     });
 }
 
@@ -23,6 +23,34 @@ function addTodo(todo) {
         todoText = todo.text;
     }
 
+    if (todoText) {
+        const todoEl = document.createElement("li");
+        if (todo && todo.completed) {
+            todoEl.classList.add("completed");
+        }
+
+        todoEl.innerText = todoText;
+
+        todoEl.addEventListener("click", () => {
+            todoEl.classList.toggle("completed");
+
+            updateLS();
+        });
+
+        todoEl.addEventListener("contextmenu", (e) => {
+            e.preventDefault();
+
+            todoEl.remove();
+
+            updateLS();
+        });
+
+        todosUL.appendChild(todoEl);
+
+        input.value = "";
+
+        updateLS();
+    }
 
 }
 
