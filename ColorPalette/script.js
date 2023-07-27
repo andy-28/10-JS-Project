aceb4ee70b183965118c793e38a24b6d4a4d4e59
andy-28 <a5706617@gmail.com>
2023-07-27 16:34:58 +0800

    commit

---
+++ ColorPalette/script.js
@@ -0,0 +1,11 @@
+const container = document.querySelector(".container");
+const refreshBtn = document.querySelector(".refresh-btn");
+
+const maxPaletteBoxes = 32;
+
+const generatePalette = () => {
+  container.innerHTML = ""; // clearing the container
+  for (let i = 0; i < maxPaletteBoxes; i++) {
+    // generating a random hex color code
+  }
+};
